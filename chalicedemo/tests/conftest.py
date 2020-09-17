f7c34f3c008f10cd592aa529ef6d931e152c63a5
crashdummymch <mihaelchoffman@gmail.com>
2020-09-16 20:56:41 -0400

    master-added new projects

---
+++ chalicedemo/tests/conftest.py
@@ -0,0 +1,34 @@
+import importlib
+import pathlib
+import pytest
+from chalice import Chalice
+
+from chalice.app import app as chalice_app
+
+
+def pytest_collection_modifyitems(config, items):
+    # python 3.4/3.5 compat: rootdir = pathlib.Path(str(config.rootdir))
+    rootdir = pathlib.Path(config.rootdir)
+    for item in items:
+        rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
+        for part in rel_path.parts:
+            if part.endswith('_tests'):
+                mark = getattr(pytest.mark, part)
+                item.add_marker(mark)
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if "data_" in fixture:
+            metafunc.parametrize(fixture, load_tests(fixture))
+
+def load_tests(name):
+    # Load module which contains test data
+    tests_module = importlib.import_module('.'+name, 'data')
+    # Tests are to be found in the variable `tests` of the module
+    for test in tests_module.tests:
+        yield test
+
+
+@pytest.fixture
+def app() -> Chalice:
+    return chalice_app
